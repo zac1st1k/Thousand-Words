90ebe721fc992377c015515c1f732fb1347332ac
zac1st1k <zacfirst@gmail.com>
2015-01-04 23:45:55 +1100

    + Update xdatamodel

---
+++ Thousand Words/Album.h
@@ -0,0 +1,18 @@
+//
+//  Album.h
+//  Thousand Words
+//
+//  Created by Zac on 4/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Album : NSManagedObject
+
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ Thousand Words/Album.m
@@ -0,0 +1,17 @@
+//
+//  Album.m
+//  Thousand Words
+//
+//  Created by Zac on 4/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import "Album.h"
+
+
+@implementation Album
+
+@dynamic date;
+@dynamic name;
+
+@end
