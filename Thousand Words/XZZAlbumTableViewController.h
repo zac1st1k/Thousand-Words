9552bbc7444fbeca21d134732b452c5e7a19cd46
zac1st1k <zacfirst@gmail.com>
2015-01-04 23:28:36 +1100

    + Empty Application + Setup storyboard + Setup UITableView

---
+++ Thousand Words/XZZAlbumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  XZZAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Zac on 4/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZAlbumTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *albums;
+
+@end
